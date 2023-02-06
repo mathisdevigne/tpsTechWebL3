cd09cfa499e0a9baf213a8a8f0aface57e923d94
DEVIGNE <62422355+mathisdevigne@users.noreply.github.com>
2023-02-06 11:55:56 +0100

    tp2

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'accueil')]
+    public function indexAction(): Response
+    {
+        return
+        $this->render('Accueil/index.html.twig');
+    }
+}
